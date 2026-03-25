8881ced9ec5888d29d0381ecabd3f1dc7bf242aa
Gustavo Pessatto Padilha <208980@upf.br>
2026-03-25 20:09:11 -0300

    rafa

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\CursoController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/teste', function () {
+    return view('teste');
+});
+Route::resource('cursos', CursoController::class);
